7de8498e5a0e507db4ad31a1752b52e35e746696
guillermouzner <guillermouz16@gmail.com>
2023-07-15 21:23:00 -0300

    x

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    serverComponentsExternalPackages: ["@prisma/client"],
-  },
 };
 
 module.exports = nextConfig;
